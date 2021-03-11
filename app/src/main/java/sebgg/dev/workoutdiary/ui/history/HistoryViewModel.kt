86f2e3e13336ee22ad1bf7924cab7bbc2daa5ccb
Sebastian Grunditz <sebgr273@student.liu.se>
2021-03-11 08:58:26 +0100

    Fix: added dummy variable to prevent crash. Fixed viewModelFactory returning wrong type of viewModel

--- app/src/main/java/sebgg/dev/workoutdiary/ui/history/HistoryViewModel.kt
+++ app/src/main/java/sebgg/dev/workoutdiary/ui/history/HistoryViewModel.kt
@@ -6,8 +6,8 @@ import sebgg.dev.workoutdiary.ui.database.ExerciseRepository
 import sebgg.dev.workoutdiary.ui.database.dao.Workout
 import sebgg.dev.workoutdiary.ui.workout.WorkoutViewModel
 
-class HistoryViewModel(private val repository: ExerciseRepository): ViewModel() {
-
+class HistoryViewModel(repository: ExerciseRepository): ViewModel() {
+    var dummy: Int = 1
     val workouts: LiveData<List<Workout>> = repository.getAllWorkouts().asLiveData()
 }
 
@@ -15,9 +15,8 @@ class HistoryViewModelFactory(private val repository: ExerciseRepository): ViewM
     override fun <T : ViewModel> create (modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(HistoryViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return WorkoutViewModel(repository) as T
+            return HistoryViewModel(repository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
-
     }
 }
